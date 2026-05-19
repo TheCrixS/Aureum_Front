b57bc6c270f03f0172675f8bffa6ecef47fd88fd
criss <csotov@ucentral.edu.co>
2026-05-18 22:23:59 -0500

    Arreglo de rutas

--- aureum/src/app/app.routes.ts
+++ aureum/src/app/app.routes.ts
@@ -4,9 +4,7 @@ import {CrearBarbero} from './components/crear-barbero/crear-barbero';
 
 export const routes: Routes = [
   { path: 'crear', component: ServicioForm },
-  { path: '', redirectTo: 'crear', pathMatch: 'full' },
-  { path: '**', redirectTo: 'crear' },
-  { path: 'crear', component: CrearBarbero },
+  { path: 'crear-barbero', component: CrearBarbero },
   {
     path: 'solicitar-cita',
     loadComponent: () =>
